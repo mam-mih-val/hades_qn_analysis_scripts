bce6051b804f08a1b4bbe27a515f4b4e5a7ee0c1
mam-mih-val <mam.mih.val@gmail.com>
2020-11-07 16:45:44 +0300

    bug fixed

--- batch_run.sh
+++ batch_run.sh
@@ -34,6 +34,6 @@ mv correction_out.root correction_in.root
 
 $build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt -t hades_analysis_tree --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/correction-agag-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C --event-cuts hades/auau/1.23/event_cuts/standard/pt2
 
-$build_dir/QnAnalysisCorrelate/QnAnalysisCorrelate --configuration-file /lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/correlation.yaml --configuration-name _tasks --input-file correction_out.root --input-tree=tree --output-file correlation_out.root
+$build_dir/QnAnalysisCorrelate/QnAnalysisCorrelate --configuration-file /lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/correlation.yml --configuration-name _tasks --input-file correction_out.root --input-tree=tree --output-file correlation_out.root
 
 echo JOB FINISHED!
