7083223028df4bac91dabc7bc2e17ce49b4e03d9
mam-mih-val <mam.mih.val@gmail.com>
2021-02-14 17:47:40 +0300

    ready for the virgo

--- auau123/batch_run.sh
+++ auau123/batch_run.sh
@@ -6,14 +6,14 @@ date $format
 
 job_num=$(($SLURM_ARRAY_TASK_ID))
 
-filelist=$lists_dir/$job_num.list
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
 
 cd $output_dir
 mkdir -p $job_num
 cd $job_num
 
 while read line; do
-    echo $line >> list.txt
+  echo $line >> list.txt
 done < $filelist
 echo >> list.txt
 
