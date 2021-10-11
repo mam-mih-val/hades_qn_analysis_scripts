da33c51de4477a7f1110dc0c9319e1c6fdc468c4
mam-mih-val <mam.mih.val@gmail.com>
2021-10-11 10:59:54 +0200

    Preparation for centos7 farm

--- mc-all-auau123/batch_run.sh
+++ mc-all-auau123/batch_run.sh
@@ -17,17 +17,13 @@ while read line; do
 done < $filelist
 echo >> list.txt
 
-source /etc/profile.d/modules.sh
-module use /cvmfs/it.gsi.de/modulefiles/
-module load compiler/gcc/9
-
-echo "loading /lustre/nyx/hades/user/mmamaev/install/root-6.18.04/cxx17/bin/thisroot.sh"
-source /lustre/nyx/hades/user/mmamaev/install/root-6.18.04/cxx17/bin/thisroot.sh
+echo "loading /lustre/nyx/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh"
+source /lustre/nyx/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh
 
 echo
 
 #/lustre/nyx/hades/user/mmamaev/hades_preprocessing/build/src/pre_process -i list.txt -t hades_analysis_tree -o reco.root --output-tree-name extra_reco -n -1 --protons-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_protons.root --pi-plus-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_pi_plus.root --pi-minus-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_pi_minus.root
-/lustre/nyx/hades/user/mmamaev/hades_preprocessing/build/src/pre_process -i list.txt -t hades_analysis_tree -o reco.root --output-tree-name extra_reco -n -1 --all-efficiency=/lustre/nyx/hades/user/mmamaev/hades_preprocessing/efficiency_files/efficiency_all.root
+/lustre/nyx/hades/user/mmamaev/hades_preprocessing/build-centos7/src/pre_process -i list.txt -t hades_analysis_tree -o reco.root --output-tree-name extra_reco -n -1
 
 date $format
 
