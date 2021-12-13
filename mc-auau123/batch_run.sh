877f36592ef27f417c77ab3ae36b2b8fdbc71f7a
mam-mih-val <mam.mih.val@gmail.com>
2021-12-13 10:49:35 +0100

    Only filling q-vectors

--- mc-auau123/batch_run.sh
+++ mc-auau123/batch_run.sh
@@ -47,18 +47,18 @@ date $format
 #$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt wall.txt reco.txt -t hades_analysis_tree reconstructed_wall extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123-true.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
 
 $build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt reco.txt -t hades_analysis_tree extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
-mv correction_out.root correction_in.root
-
-date $format
-
-$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt reco.txt -t hades_analysis_tree extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
-mv correction_out.root correction_in.root
+#mv correction_out.root correction_in.root
 #
-date $format
-
-$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt reco.txt -t hades_analysis_tree extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
-
-date $format
+#date $format
+#
+#$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt reco.txt -t hades_analysis_tree extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
+#mv correction_out.root correction_in.root
+##
+#date $format
+#
+#$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt reco.txt -t hades_analysis_tree extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C
+#
+#date $format
 
 $build_dir/QnAnalysisCorrelate/QnAnalysisCorrelate --configuration-file /lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correlation.yml --configuration-name _tasks --input-file correction_out.root --input-tree=tree --output-file correlation_out.root
 
