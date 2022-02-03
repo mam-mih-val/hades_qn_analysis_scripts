b6b95d78077f8183583093930762241df43c4a7b
mam-mih-val <mam.mih.val@gmail.com>
2022-02-03 16:14:52 +0100

    Test run

---
+++ mc-auau123-wall/batch_run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+job_num=$(($SLURM_ARRAY_TASK_ID))
+
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
+
+cd $output_dir
+mkdir -p $job_num
+cd $job_num
+
+while read line; do
+  echo $line >> list.txt
+done < $filelist
+echo >> list.txt
+
+while read line; do
+  directory=`dirname $line`
+  echo $directory/wall_output.root >> wall.txt
+done < $filelist
+echo >> wall.txt
+
+echo "loading  /lustre/nyx/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh"
+source /lustre/nyx/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh
+
+echo
+
+date $format
+
+/lustre/nyx/hades/user/mmamaev/hades_preprocessing/build-centos7/src/pre_process -i list.txt \
+                                                                                -t hades_analysis_tree \
+                                                                                -o reco.root \
+                                                                                --output-tree-name extra_reco \
+                                                                                -n -1 \
+                                                                                --protons-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_protons.root \
+                                                                                --pi-plus-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_pi_plus.root \
+                                                                                --pi-minus-efficiency=/lustre/nyx/hades/user/mmamaev/hades_rapidity/efficiency_files/efficiency_pi_minus.root \
+                                                                                --efficiency-delta-phi=/lustre/nyx/hades/user/mmamaev/hades_preprocessing/efficiency_files/delta_phi_au123_urqmd_2212_2022_01_20.root
+
+echo "loading " $ownroot
+source $ownroot
+
+current_dir=`pwd`
+find $current_dir -name "reco.root" > reco.txt
+
+date $format
+
+echo "executing $build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt wall.txt reco.txt -t hades_analysis_tree reconstructed_wall extra_reco --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correction-auau-123.yml --yaml-config-name=hades_analysis -n -1 --cuts-macro Hades/AuAu1.23.C"
+
+date $format
+
+$build_dir/QnAnalysisCorrect/QnAnalysisCorrect -i list.txt wall.txt reco.txt \
+                                              -t hades_analysis_tree reconstructed_wall extra_reco \
+                                              --yaml-config-file=/lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/mc-correlation-auau-123-wall.yml \
+                                              --yaml-config-name=hades_analysis \
+                                              -n -1 \
+                                              --cuts-macro Hades/AuAu1.23.C
+#mv correction_out.root correction_in.root
+
+$build_dir/QnAnalysisCorrelate/QnAnalysisCorrelate --configuration-file /lustre/nyx/hades/user/mmamaev/QnAnalysis/setups/hades/correlation-auau-123-occupancy.yml \
+                                                  --configuration-name _tasks \
+                                                  --input-file correction_out.root \
+                                                  --input-tree=tree \
+                                                  --output-file correlation_out.root
+
+rm reco.root wall.txt list.txt
+
+date $format
+
+echo JOB FINISHED!
---
+++ mc-auau123-wall/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+n_files=$1
+file_list=$2
+output_dir=$3
+
+ownroot=/lustre/nyx/hades/user/mmamaev/install/root-6.20.04-centos7-cxx17/bin/thisroot.sh
+
+current_dir=$(pwd)
+partition=main
+time=8:00:00
+build_dir=/lustre/nyx/hades/user/mmamaev/QnAnalysis/build-centos7/src
+
+lists_dir=${output_dir}/lists/
+log_dir=${output_dir}/log/
+
+mkdir -p $output_dir
+mkdir -p $log_dir
+mkdir -p $lists_dir
+
+split -l $n_files -d -a 3 --additional-suffix=.list "$file_list" $lists_dir
+
+n_runs=$(ls $lists_dir/*.list | wc -l)
+
+job_range=1-$n_runs
+
+echo file list=$file_list
+echo output_dir=$output_dir
+echo log_dir=$log_dir
+echo lists_dir=$lists_dir
+echo n_runs=$n_runs
+echo job_range=$job_range
+
+sbatch --wait \
+        -J QnAnalysis \
+        -p $partition \
+        -t $time \
+        -a $job_range \
+        -e ${log_dir}/%A_%a.e \
+        -o ${log_dir}/%A_%a.o \
+        --export=output_dir=$output_dir,file_list=$file_list,ownroot=$ownroot,lists_dir=$lists_dir,build_dir=$build_dir \
+        -- /lustre/nyx/hades/user/mmamaev/hades_qn_analysis_scripts/mc-auau123/batch_run.sh
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/nyx/hades/user/mmamaev/install/QnTools/lib
+source $ownroot
+
+hadd -j -f $output_dir/correlation_all.root $output_dir/*/correlation_out.root >& $log_dir/log_merge_$STEP.txt
+
+echo JOBS HAVE BEEN COMPLETED!
